daa02bd9017fa0426547928bb63b06eec56d0f8b
andy <andyshenproper@gmail.com>
2020-01-23 18:27:37 -0800

    Change Gallery.java to Bounty.java and fix other file references

--- app/src/main/java/ca/vikingsdev/centauri/ui/bounties/BountyFragment.java
+++ app/src/main/java/ca/vikingsdev/centauri/ui/bounties/BountyFragment.java
@@ -1,4 +1,4 @@
-package ca.vikingsdev.centauri.ui.gallery;
+package ca.vikingsdev.centauri.ui.bounties;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import ca.vikingsdev.centauri.R;
 
-public class GalleryFragment extends Fragment {
+public class BountyFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+    private BountyViewModel bountyViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        bountyViewModel =
+                ViewModelProviders.of(this).get(BountyViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_bounties, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
+        bountyViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/ca/vikingsdev/centauri/ui/bounties/BountyViewModel.java
+++ app/src/main/java/ca/vikingsdev/centauri/ui/bounties/BountyViewModel.java
@@ -1,16 +1,16 @@
-package ca.vikingsdev.centauri.ui.gallery;
+package ca.vikingsdev.centauri.ui.bounties;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class BountyViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public BountyViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is bounty fragment");
     }
 
     public LiveData<String> getText() {
